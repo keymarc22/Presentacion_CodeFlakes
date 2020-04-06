62b76da96c8d31997212cd710c3874a78993da24
Keymar Cloralt <60714375+keymarc22@users.noreply.github.com>
2020-04-06 12:56:35 -0400

    Add files via upload

---
+++ Presentacion_CodeFlakes.js
@@ -0,0 +1,24 @@
+var numero = prompt("Hola Bienvenido!! Escoge un Numero")
+var div
+
+function esdivisible (n, div){
+  if(n % div == 0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+for(i=1; i<= numero; i++){
+  if(esdivisible(i,3)){
+    document.write("agua")
+  }
+  if(esdivisible(i,5)){
+    document.write("cate")
+  }
+  if(!esdivisible(i,3)&&!esdivisible(i,5)){
+    document.write(i)
+  }
+  document.write("<br />");
+}
